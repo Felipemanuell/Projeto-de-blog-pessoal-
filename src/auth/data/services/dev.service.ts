5d862b1ba581c2278be92f60ab62758132b3be0b
Felipe Emanuel <fe806497@gmail.com>
2025-10-15 13:03:01 -0300

    Consertado erros na dev.service.ts

--- src/auth/data/services/dev.service.ts
+++ src/auth/data/services/dev.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { Postagem } from "../../postagem/entities/postagem.entity";
-import { Tema } from "../../tema/entities/tema.entity";
-import { Usuario } from "../../usuario/entities/usuario.entity";
+import { Postagem } from "../../../postagem/entities/postagem.entity";
+import { Tema } from "../../../tema/entities/tema.entity";
+import { Usuario } from "../../../usuario/entities/usuario.entity";
 
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
@@ -14,7 +14,7 @@ export class DevService implements TypeOrmOptionsFactory {
             port: 3306,
             username: 'root',
             password: 'root',
-            database: 'db_blogpessoal',
+            database: 'db_blog_de_beachcity',
             entities: [Postagem, Tema, Usuario],
             synchronize: true,
     };
