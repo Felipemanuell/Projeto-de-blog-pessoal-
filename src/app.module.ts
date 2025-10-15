403e6fec3aa7f05705807edf2912c66c645f1a77
Felipe Emanuel <fe806497@gmail.com>
2025-10-15 12:52:26 -0300

    Deploy do Projeto blog_de_beachcity

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './auth/data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blog_de_beachcity',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-      logging: true,
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	  useClass: ProdService,
+    imports: [ConfigModule],
+}),
     PostagemModule,
     TemaModule,
     AuthModule,
